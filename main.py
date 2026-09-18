fd77e030ca14386596240886d0dc82825716610d
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-09-18 10:21:53 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('check back later ;)')
+import urllib.parse
+
+user_url = input('Enter url: ')
+
+parsed = urllib.parse.urlparse(user_url)
+
+if parsed.scheme and parsed.netloc:
+    print('j')
+else:
+    print('jjj')
+ 
