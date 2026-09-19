ff41e6e62c1f12dd6c2b948f63ef9ff9f8b86271
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-09-19 08:58:46 -0700

    collision checks, regenerate and store both urls in db

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ while True:
 
          
 characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-char = []
 
-for _ in range(6):
-    random_str = secrets.choice(characters)
-    char.append(random_str)
+def generate_code():
+    char = []   
+    for _ in range(6):
+        random_str = secrets.choice(characters)
+        char.append(random_str)
 
-short_code = ''.join(char)
-print(short_code)
+    short_code = ''.join(char)
+    return short_code
+
 
 conn = sqlite3.connect('url.db')
 cursor = conn.cursor()
@@ -28,23 +30,34 @@ cursor.execute("""
     CREATE TABLE IF NOT EXISTS urls(
         original_url TEXT,
         short_code TEXT
-    )
-""")
+        )
+    """)
 
 conn.commit()
 
+
+while True:
+    short_code = generate_code()
+
+    cursor.execute("""
+        SELECT * FROM urls 
+        WHERE short_code = ?;
+    """, (short_code,))
+
+    result = cursor.fetchone()
+
+    if result is not None:
+        continue
+
+    if result is None:
+        break
+ 
 cursor.execute("""
     INSERT INTO urls(original_url, short_code)
     VALUES (?, ?)
-""", (user_url, short_code))
+    """, (user_url, short_code))
 
 conn.commit()
 
-cursor.execute("""
-    SELECT * FROM urls 
-    WHERE short_code = ?;
-""", (short_code,))
-
-result = cursor.fetchone()
-print(result)
+print(short_code)
 
