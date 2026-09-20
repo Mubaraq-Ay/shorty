21be84e8cad381e2533db30f4b607648ef041aee
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-09-19 20:43:15 -0700

    add interactive cli interface.

--- main.py
+++ main.py
@@ -2,62 +2,87 @@ import urllib.parse
 import secrets
 import sqlite3
 
-while True:
-    user_url = input('Enter url: ')
-    parsed = urllib.parse.urlparse(user_url)
-    if parsed.scheme in ['http', 'https'] and parsed.netloc:
-        break
-    else:
-        print('Invalid URL, kindly enter a valid one.')
 
-         
-characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+print("""
+----- SHORTY -----
 
-def generate_code():
-    char = []   
-    for _ in range(6):
-        random_str = secrets.choice(characters)
-        char.append(random_str)
+Select an option:
 
-    short_code = ''.join(char)
-    return short_code
+1. Shorten URL
+2. Exit
+""")
 
 
-conn = sqlite3.connect('url.db')
-cursor = conn.cursor()
+choice = input("Select an option: ")
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS urls(
-        original_url TEXT,
-        short_code TEXT
+if choice == "1":
+
+    while True:
+        user_url = input("Enter URL: ")
+        parsed = urllib.parse.urlparse(user_url)
+
+        if parsed.scheme in ['http', 'https'] and parsed.netloc:
+            break
+
+        print("Invalid URL, kindly enter a valid one.")
+
+
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+
+    def generate_code():
+        char = []
+
+        for _ in range(6):
+            random_str = secrets.choice(characters)
+            char.append(random_str)
+
+        short_code = ''.join(char)
+        return short_code
+
+
+    conn = sqlite3.connect('url.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS urls(
+            original_url TEXT,
+            short_code TEXT
         )
     """)
 
-conn.commit()
+    conn.commit()
 
 
-while True:
-    short_code = generate_code()
+    while True:
+        short_code = generate_code()
+
+        cursor.execute("""
+            SELECT * FROM urls
+            WHERE short_code = ?;
+        """, (short_code,))
+
+        result = cursor.fetchone()
+
+        if result is not None:
+            continue
+
+        break
+
 
     cursor.execute("""
-        SELECT * FROM urls 
-        WHERE short_code = ?;
-    """, (short_code,))
-
-    result = cursor.fetchone()
-
-    if result is not None:
-        continue
-
-    if result is None:
-        break
- 
-cursor.execute("""
-    INSERT INTO urls(original_url, short_code)
-    VALUES (?, ?)
+        INSERT INTO urls(original_url, short_code)
+        VALUES (?, ?)
     """, (user_url, short_code))
 
-conn.commit()
+    conn.commit()
 
-print(short_code)
+    print(short_code)
 
+
+elif choice == "2":
+    print("Okay, bye.")
+
+
+else:
+    print("Invalid option.")
